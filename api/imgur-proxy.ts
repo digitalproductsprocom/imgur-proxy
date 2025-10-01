8390042a655c81e4ea060b13270ee82f87fb0cd6
digitalproductsprocom <digitalproductspro.com@gmail.com>
2025-10-01 22:48:15 +0100

    Create imgur-proxy.ts

---
+++ api/imgur-proxy.ts
@@ -0,0 +1,29 @@
+// api/imgur-proxy.ts
+export default async function handler(req: any, res: any) {
+  const target = (req.query?.url || req.query?.u) as string;
+  if (!target) return res.status(400).send("Missing ?url");
+
+  let u: URL;
+  try { 
+    u = new URL(target); 
+  } catch { 
+    return res.status(400).send("Bad url"); 
+  }
+
+  const allowed = new Set([
+    "i.imgur.com",
+    "imgur.com",
+    "m.imgur.com",
+    "i.stack.imgur.com"
+  ]);
+  if (!allowed.has(u.hostname)) {
+    return res.status(403).send("Host not allowed");
+  }
+
+  try {
+    const r = await fetch(u.toString(), {
+      headers: {
+        "User-Agent": "Imgur-Proxy/1.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://
